c689029234742d0c67a6276ed4338e58d639eefe
joseasantacruz <joseasantacruz@gmail.com>
2014-04-27 16:46:04 -0400

    Fase ok

--- todo/admin.py
+++ todo/admin.py
@@ -7,25 +7,25 @@ class FaseAdmin(admin.TabularInline):
     """
     Definicion de la clase FaseAdmin
     """
-    list_display = ('Nombre',)
-    list_filter = ('Nombre',)
+    list_display = ('Proyecto', 'Nombre', "Descripcion", "FechaCreacion",)
+    list_filter = ( 'FechaCreacion', 'FechaInicio', 'Estado',)
     search_fields = ['Nombre']
+    ordering = ('Nombre',)
     model = Fase
     extra = 0
 
 
 class ProyectoAdmin(admin.ModelAdmin):
     """
-    Definicion de la clase de Administracion de Proyecto
+    Definicion de la clase Proyecto
     """
     list_display = ('Nombre', "Descripcion", "FechaCreacion",)
-    list_filter = ('Nombre', "FechaCreacion",)
+    list_filter = ( 'FechaCreacion', 'FechaInicio', 'Estado',)
     search_fields = ['Nombre']
     ordering = ('Nombre',)
-    #inlines = [FaseAdmin]
-
+    inlines = [FaseAdmin]
 
 
 admin.site.register(Proyecto, ProyectoAdmin)
-#admin.site.register(Fase)
+admin.site.register(Fase)
 
--- todo/models.py
+++ todo/models.py
@@ -6,7 +6,9 @@ from django.db import models
 
 
 
-#Clase Proyecto, define los campos y textos que contienen los datos del Proyecto
+"""Models contiene los modelos del proyecto"""
+
+
 class Proyecto(models.Model):
     """
     Clase Proyecto
@@ -14,33 +16,40 @@ class Proyecto(models.Model):
     """
 
     Nombre = models.CharField('Nombre', max_length=45, help_text='Ingrese el nombre del proyecto')
+    """Nombre del Proyecto"""
     Descripcion = models.CharField('Descripcion', max_length=45, help_text='Ingrese la descripcion del proyecto')
+    """Descripción del Proyecto"""
     FechaCreacion = models.DateField('Fecha de Creacion', help_text='Ingrese la fecha de creacion del proyecto')
+    """Descripción del Proyecto"""
     FechaInicio = models.DateField('Fecha de Inicio', help_text='Ingrese la fecha de Inicio del proyecto', null=True,
                                    blank=True)
+    """Fecha de inicio del Proyecto"""
     FechaFin = models.DateField('Fecha de Fin', help_text='Ingrese la fecha de Fin del proyecto', null=True, blank=True)
+    """Fecha de finalización del Proyecto"""
     Estado = models.CharField('Estado', max_length=45, help_text='Ingrese el Estado del proyecto', null=True,
                               blank=True)
+    """Estado del Proyecto"""
 
     def __unicode__(self):
         return self.Nombre
 
 
     class Meta:
-        #en esta clase definimos que se listaran los proyectos ordenados por el nombre
+        """En esta clase definimos que se listaran los proyectos ordenados por el nombre"""
         ordering = ('Nombre',)
         permissions = (
             ("ver proyecto", "Puede visualizar el proyecto"),
             ("iniciar proyecto", "Puede iniciar el proyecto"),
+            ("administrar proyecto designadoo", "Puede administrar el proyecto que le fue designado"),
         )
 
 #Clase Fase, define los campos y textos que contienen los datos de cada Fase
 class Fase(models.Model):
     """
     Clase Fase
-    Define los campos y textos que continen los datos de cada Fase
+    Definimos los atributos de la clase Fase
     """
-    fkproyecto = models.ForeignKey(Proyecto)
+    Proyecto = models.ForeignKey(Proyecto)
     Nombre = models.CharField('Nombre', max_length=45, help_text='Ingrese el nombre de la fase')
     NroOrden = models.IntegerField(help_text='Ingrese el numero de orden de la fase')
     Descripcion = models.CharField('Descripcion', max_length=45, help_text='Ingrese la descripcion de la fase')
@@ -51,9 +60,10 @@ class Fase(models.Model):
     Estado = models.CharField('Estado', max_length=45, help_text='Ingrese el Estado de la fase', null=True, blank=True)
 
     def __unicode__(self):
-        return u'%s | %s' % (self.fkproyecto, self.Nombre)
+        return u'%s | %s' % (self.Proyecto, self.Nombre)
 
     class Meta:
+        """En esta clase definimos que se listaran los proyectos ordenados por el nombre"""
         verbose_name = u'Fase'
         verbose_name_plural = 'Fases'
 
