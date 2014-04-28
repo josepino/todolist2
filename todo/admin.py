503d9eef81d5d4a1e2d3577218906507938c9635
joseasantacruz <joseasantacruz@gmail.com>
2014-04-28 01:26:04 -0400

    Fase y tipoItem

--- todo/admin.py
+++ todo/admin.py
@@ -3,11 +3,21 @@ from django.contrib import admin
 from models import *
 
 
+class TipoItemAdmin(admin.ModelAdmin):
+    """
+    Definicion de la clase TipoItemAdmin
+    """
+    list_display = ("Nombre", "Descripcion", "Fase",)
+    list_filter = ( 'Fase',)
+    search_fields = ['Nombre']
+    ordering = ('Nombre',)
+
+
 class FaseAdmin(admin.TabularInline):
     """
     Definicion de la clase FaseAdmin
     """
-    list_display = ('Proyecto', 'Nombre', "Descripcion", "FechaCreacion",)
+    list_display = ("Nombre", "Descripcion", "FechaCreacion",)
     list_filter = ( 'FechaCreacion', 'FechaInicio', 'Estado',)
     search_fields = ['Nombre']
     ordering = ('Nombre',)
@@ -17,9 +27,9 @@ class FaseAdmin(admin.TabularInline):
 
 class ProyectoAdmin(admin.ModelAdmin):
     """
-    Definicion de la clase Proyecto
+    Definicion de la clase ProyectoAdmin
     """
-    list_display = ('Nombre', "Descripcion", "FechaCreacion",)
+    list_display = ("Nombre", "Descripcion", "FechaCreacion",)
     list_filter = ( 'FechaCreacion', 'FechaInicio', 'Estado',)
     search_fields = ['Nombre']
     ordering = ('Nombre',)
@@ -28,4 +38,5 @@ class ProyectoAdmin(admin.ModelAdmin):
 
 admin.site.register(Proyecto, ProyectoAdmin)
 admin.site.register(Fase)
+admin.site.register(TipoItem, TipoItemAdmin)
 
--- todo/models.py
+++ todo/models.py
@@ -20,7 +20,7 @@ class Proyecto(models.Model):
     Descripcion = models.CharField('Descripcion', max_length=45, help_text='Ingrese la descripcion del proyecto')
     """Descripción del Proyecto"""
     FechaCreacion = models.DateField('Fecha de Creacion', help_text='Ingrese la fecha de creacion del proyecto')
-    """Descripción del Proyecto"""
+    """Fecha de creación del Proyecto"""
     FechaInicio = models.DateField('Fecha de Inicio', help_text='Ingrese la fecha de Inicio del proyecto', null=True,
                                    blank=True)
     """Fecha de inicio del Proyecto"""
@@ -49,21 +49,51 @@ class Fase(models.Model):
     Clase Fase
     Definimos los atributos de la clase Fase
     """
-    Proyecto = models.ForeignKey(Proyecto)
+    fkproyecto = models.ForeignKey(Proyecto)
+    """Nombre del Proyecto"""
     Nombre = models.CharField('Nombre', max_length=45, help_text='Ingrese el nombre de la fase')
+    """Nombre de la Fase"""
     NroOrden = models.IntegerField(help_text='Ingrese el numero de orden de la fase')
+    """Numero de orden de la Fase"""
     Descripcion = models.CharField('Descripcion', max_length=45, help_text='Ingrese la descripcion de la fase')
+    """Descripción de la Fase"""
     FechaCreacion = models.DateField('Fecha de Creacion', help_text='Ingrese la fecha de creacion de la fase')
+    """Fecha de creación de la Fase"""
     FechaInicio = models.DateField('Fecha de Inicio', help_text='Ingrese la fecha de Inicio de la fase', null=True,
                                    blank=True)
+    """Fecha de inicio de la Fase"""
     FechaFin = models.DateField('Fecha de Fin', help_text='Ingrese la fecha de Fin de la fase', null=True, blank=True)
+    """Fecha de finalizaciónde la Fase"""
     Estado = models.CharField('Estado', max_length=45, help_text='Ingrese el Estado de la fase', null=True, blank=True)
+    """Estado de la Fase"""
 
     def __unicode__(self):
-        return u'%s | %s' % (self.Proyecto, self.Nombre)
+        return u'%s | %s' % (self.fkproyecto, self.Nombre)
 
     class Meta:
-        """En esta clase definimos que se listaran los proyectos ordenados por el nombre"""
+        """En esta clase definimos que se listaran las fases ordenados por el nombre"""
+        ordering = ('Nombre',)
         verbose_name = u'Fase'
         verbose_name_plural = 'Fases'
 
+
+class TipoItem(models.Model):
+    """
+    Clase TipoItem
+    Definimos los atributos de la clase TipoItem
+    """
+    Fase = models.ForeignKey(Fase)
+    Nombre = models.CharField('Nombre', max_length=45, help_text='Ingrese el nombre del Tipo de Item')
+    """Nombre del Tipo de Item"""
+    Descripcion = models.CharField('Descripcion', max_length=45,
+                                   help_text='Ingrese la descripcion del del Tipo de Item')
+    """Descripción del Tipo de Item"""
+
+    def __unicode__(self):
+        return u'%s | %s' % (self.Fase, self.Nombre)
+
+    class Meta:
+        """En esta clase definimos que se listaran los Tipos de Item por el nombre"""
+        ordering = ('Nombre',)
+        verbose_name = u'TipoItem'
+        verbose_name_plural = 'Tipos de Item'
