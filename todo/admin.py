130755daa5b60e81d6c0d3e77787f268496c4b03
joseasantacruz <joseasantacruz@gmail.com>
2014-04-28 01:46:37 -0400

    Arreglo en Fase y tipoItem

--- todo/admin.py
+++ todo/admin.py
@@ -7,19 +7,36 @@ class TipoItemAdmin(admin.ModelAdmin):
     """
     Definicion de la clase TipoItemAdmin
     """
-    list_display = ("Nombre", "Descripcion", "Fase",)
+    list_display = ('Nombre', 'Fase', 'Descripcion',)
     list_filter = ( 'Fase',)
     search_fields = ['Nombre']
     ordering = ('Nombre',)
 
 
-class FaseAdmin(admin.TabularInline):
+class TipoItemAdmin2(admin.TabularInline):
+    """
+    Definicion de la clase TipoItemAdmin2 que se utiliza para la clase FaseAdmin
+    """
+    ordering = ('Nombre',)
+    model = TipoItem
+    extra = 0
+
+
+class FaseAdmin(admin.ModelAdmin):
     """
     Definicion de la clase FaseAdmin
     """
-    list_display = ("Nombre", "Descripcion", "FechaCreacion",)
-    list_filter = ( 'FechaCreacion', 'FechaInicio', 'Estado',)
+    list_display = ('Nombre', 'fkproyecto', 'Descripcion', 'FechaCreacion',)
+    list_filter = ( 'fkproyecto', 'FechaCreacion', 'FechaInicio', 'Estado',)
     search_fields = ['Nombre']
+    ordering = ('Nombre',)
+    inlines = [TipoItemAdmin2]
+
+
+class FaseAdmin2(admin.TabularInline):
+    """
+    Definicion de la clase FaseAdmin2 que se utiliza para la clase ProyectoAdmin
+    """
     ordering = ('Nombre',)
     model = Fase
     extra = 0
@@ -29,14 +46,16 @@ class ProyectoAdmin(admin.ModelAdmin):
     """
     Definicion de la clase ProyectoAdmin
     """
-    list_display = ("Nombre", "Descripcion", "FechaCreacion",)
+    list_display = ('Nombre', 'Descripcion', 'FechaCreacion',)
     list_filter = ( 'FechaCreacion', 'FechaInicio', 'Estado',)
     search_fields = ['Nombre']
     ordering = ('Nombre',)
-    inlines = [FaseAdmin]
+    inlines = [FaseAdmin2]
 
 
-admin.site.register(Proyecto, ProyectoAdmin)
-admin.site.register(Fase)
 admin.site.register(TipoItem, TipoItemAdmin)
+admin.site.register(Fase, FaseAdmin)
+admin.site.register(Proyecto, ProyectoAdmin)
+
+
 
