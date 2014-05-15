7d40c02ae0f8218660fe943686b48815ee79ff34
joseasantacruz <joseasantacruz@gmail.com>
2014-05-15 17:15:12 -0300

    abm linea base ok

--- todo/admin.py
+++ todo/admin.py
@@ -7,37 +7,37 @@ class LineaBaseAdmin(admin.ModelAdmin):
     """
     Definicion de la clase LineaBaseAdmin
     """
-    list_display = ('Nombre', 'Fase', 'FechaCreacion', 'Estado',)
-    list_filter = ( 'Fase', 'FechaCreacion', 'Estado',)
-    search_fields = ['Nombre']
-    ordering = ('Nombre',)
+    list_display = ('nombre', 'fase', 'fechacreacion', 'estado',)
+    list_filter = ( 'fase', 'fechacreacion', 'estado',)
+    search_fields = ['nombre']
+    ordering = ('nombre',)
 
 
 class RelacionAdmin(admin.ModelAdmin):
     """
     Definicion de la clase RelacionAdmin
     """
-    list_display = ('ItemOrigen', 'TipoRelacion', 'ItemDestino',)
-    list_filter = ('ItemOrigen', 'TipoRelacion', 'ItemDestino',)
-    search_fields = ['ItemOrigen']
-    ordering = ('ItemOrigen', 'ItemDestino',)
+    list_display = ('itemorigen', 'tiporelacion', 'itemdestino',)
+    list_filter = ('itemorigen', 'tiporelacion', 'itemdestino',)
+    search_fields = ['itemorigen']
+    ordering = ('itemorigen', 'itemdestino',)
 
 
 class AtributoItemAdmin(admin.ModelAdmin):
     """
     Definicion de la clase AtributoItemAdmin
     """
-    list_display = ('Nombre', 'Item', 'AtributoTipoItem', 'Descripcion',)
-    list_filter = ( 'Item', 'AtributoTipoItem',)
-    search_fields = ['Nombre']
-    ordering = ('Nombre',)
+    list_display = ('nombre', 'item', 'atributotipoitem', 'descripcion',)
+    list_filter = ( 'item', 'atributotipoitem',)
+    search_fields = ['nombre']
+    ordering = ('nombre',)
 
 
 class AtributoItemAdmin2(admin.TabularInline):
     """
     Definicion de la clase AtributoItemAdmin2 que se utiliza para la clase ItemAdmin
     """
-    ordering = ('Nombre',)
+    ordering = ('nombre',)
     model = AtributoItem
     extra = 0
 
@@ -47,10 +47,10 @@ class ItemAdmin(admin.ModelAdmin):
     Definicion de la clase ItemAdmin
     """
     list_display = (
-    'Nombre', 'TipoItem', 'Descripcion', 'Complejidad', 'Estado', 'Version', 'Costo', 'FechaModificacion',)
-    list_filter = ( 'TipoItem', 'Complejidad', 'Estado', 'Costo', 'FechaModificacion',)
-    search_fields = ['Nombre']
-    ordering = ('Nombre',)
+        'nombre', 'tipoitem', 'descripcion', 'complejidad', 'estado', 'version', 'costo', 'fechamodificacion',)
+    list_filter = ( 'tipoitem', 'complejidad', 'estado', 'costo', 'fechamodificacion',)
+    search_fields = ['nombre']
+    ordering = ('nombre',)
     inlines = [AtributoItemAdmin2]
 
 
@@ -58,7 +58,7 @@ class ItemAdmin2(admin.TabularInline):
     """
     Definicion de la clase ItemAdmin2 que se utiliza para la clase FaseAdmin
     """
-    ordering = ('Nombre',)
+    ordering = ('nombre',)
     model = Item
     extra = 0
 
@@ -68,17 +68,17 @@ class AtributoTipoItemAdmin(admin.ModelAdmin):
     """
     Definicion de la clase AtributoTipoItemAdmin
     """
-    list_display = ('Nombre', 'TipoItem', 'Descripcion',)
-    list_filter = ( 'TipoItem',)
-    search_fields = ['Nombre']
-    ordering = ('Nombre',)
+    list_display = ('nombre', 'tipoitem', 'descripcion',)
+    list_filter = ( 'tipoitem',)
+    search_fields = ['nombre']
+    ordering = ('nombre',)
 
 
 class AtributoTipoItemAdmin2(admin.TabularInline):
     """
     Definicion de la clase AtributoTipoItemAdmin2 que se utiliza para la clase TipoItemAdmin
     """
-    ordering = ('Nombre',)
+    ordering = ('nombre',)
     model = AtributoTipoItem
     extra = 0
 
@@ -87,10 +87,10 @@ class TipoItemAdmin(admin.ModelAdmin):
     """
     Definicion de la clase TipoItemAdmin
     """
-    list_display = ('Nombre', 'Fase', 'Descripcion',)
-    list_filter = ( 'Fase',)
-    search_fields = ['Nombre']
-    ordering = ('Nombre',)
+    list_display = ('nombre', 'fase', 'descripcion',)
+    list_filter = ( 'fase',)
+    search_fields = ['nombre']
+    ordering = ('nombre',)
     inlines = [ItemAdmin2, AtributoTipoItemAdmin2]
 
 
@@ -98,7 +98,7 @@ class TipoItemAdmin2(admin.TabularInline):
     """
     Definicion de la clase TipoItemAdmin2 que se utiliza para la clase FaseAdmin
     """
-    ordering = ('Nombre',)
+    ordering = ('nombre',)
     model = TipoItem
     extra = 0
 
@@ -107,10 +107,10 @@ class FaseAdmin(admin.ModelAdmin):
     """
     Definicion de la clase FaseAdmin
     """
-    list_display = ('Nombre', 'fkproyecto', 'Descripcion', 'FechaCreacion',)
-    list_filter = ( 'fkproyecto', 'FechaCreacion', 'FechaInicio', 'Estado',)
-    search_fields = ['Nombre']
-    ordering = ('Nombre',)
+    list_display = ('nombre', 'fkproyecto', 'descripcion', 'fechacreacion',)
+    list_filter = ( 'fkproyecto', 'fechacreacion', 'fechainicio', 'estado',)
+    search_fields = ['nombre']
+    ordering = ('nombre',)
     inlines = [TipoItemAdmin2]
 
 
@@ -118,7 +118,7 @@ class FaseAdmin2(admin.TabularInline):
     """
     Definicion de la clase FaseAdmin2 que se utiliza para la clase ProyectoAdmin
     """
-    ordering = ('Nombre',)
+    ordering = ('nombre',)
     model = Fase
     extra = 0
 
@@ -127,10 +127,10 @@ class ProyectoAdmin(admin.ModelAdmin):
     """
     Definicion de la clase ProyectoAdmin
     """
-    list_display = ('Nombre', 'Descripcion', 'FechaCreacion', 'FechaInicio', 'FechaFin',)
-    list_filter = ( 'FechaCreacion', 'FechaInicio', 'Estado',)
-    search_fields = ['Nombre']
-    ordering = ('Nombre',)
+    list_display = ('nombre', 'descripcion', 'fechacreacion', 'fechainicio', 'fechafin',)
+    list_filter = ( 'fechacreacion', 'fechainicio', 'estado',)
+    search_fields = ['nombre']
+    ordering = ('nombre',)
     inlines = [FaseAdmin2]
 
 
--- todo/models.py
+++ todo/models.py
@@ -14,16 +14,16 @@ class Proyecto(models.Model):
     Definimos los atributos de la clase Proyecto
     """
 
-    Nombre = models.CharField('Nombre', max_length=45, help_text='Ingrese el nombre del proyecto')
+    nombre = models.CharField('Nombre', max_length=45, help_text='Ingrese el nombre del proyecto')
     """Nombre del Proyecto"""
-    Descripcion = models.CharField('Descripcion', max_length=45, help_text='Ingrese la descripcion del proyecto')
+    descripcion = models.CharField('Descripcion', max_length=45, help_text='Ingrese la descripcion del proyecto')
     """Descripción del Proyecto"""
-    FechaCreacion = models.DateField('Fecha de Creacion', help_text='Ingrese la fecha de creacion del proyecto')
+    fechacreacion = models.DateField('Fecha de Creacion', help_text='Ingrese la fecha de creacion del proyecto')
     """Fecha de creación del Proyecto"""
-    FechaInicio = models.DateField('Fecha de Inicio', help_text='Ingrese la fecha de Inicio del proyecto', null=True,
+    fechainicio = models.DateField('Fecha de Inicio', help_text='Ingrese la fecha de Inicio del proyecto', null=True,
                                    editable=False, blank=True)
     """Fecha de inicio del Proyecto"""
-    FechaFin = models.DateField('Fecha de Fin', help_text='Ingrese la fecha de Fin del proyecto', null=True, blank=True,
+    fechafin = models.DateField('Fecha de Fin', help_text='Ingrese la fecha de Fin del proyecto', null=True, blank=True,
                                 editable=False)
     """Fecha de finalización del Proyecto"""
     Estado_CHOICES = (
@@ -31,26 +31,26 @@ class Proyecto(models.Model):
         ('A', 'Iniciado/Activo'),
         ('T', 'Terminado'),
     )
-    Estado = models.CharField('Estado', max_length=1, choices=Estado_CHOICES,
+    estado = models.CharField('Estado', max_length=1, choices=Estado_CHOICES,
                               help_text='Ingrese el Estado del proyecto', null=True,
                               blank=True, default='I')
     """Estado del Proyecto"""
 
     def iniciar(self):
-        self.FechaInicio = datetime.datetime.now()
+        self.fechainicio = datetime.datetime.now()
 
     def __unicode__(self):
         """En esta clase definimos como se vera a la instancia de la clase Proyecto"""
-        return self.Nombre
+        return self.nombre
 
 
     class Meta:
         """En esta clase definimos que se listaran los proyectos ordenados por el nombre"""
-        ordering = ('Nombre',)
+        ordering = ('nombre',)
         permissions = (
-            ("ver proyecto", "Puede visualizar el proyecto"),
-            ("iniciar proyecto", "Puede iniciar el proyecto"),
-            ("administrar proyecto designadoo", "Puede administrar el proyecto que le fue designado"),
+            ("ver_proyecto", "Puede visualizar el proyecto"),
+            ("iniciar_proyecto", "Puede iniciar el proyecto"),
+            ("administrar_proyecto_designado", "Puede administrar el proyecto que le fue designado"),
         )
 
 
@@ -62,36 +62,36 @@ class Fase(models.Model):
     """
     fkproyecto = models.ForeignKey(Proyecto, verbose_name="Proyecto", help_text='Seleccione el proyecto')
     """Nombre del Proyecto"""
-    Nombre = models.CharField('Nombre', max_length=45, help_text='Ingrese el nombre de la fase')
+    nombre = models.CharField('Nombre', max_length=45, help_text='Ingrese el nombre de la fase')
     """Nombre de la Fase"""
-    NroOrden = models.IntegerField(help_text='Ingrese el numero de orden de la fase')
+    nroorden = models.IntegerField(help_text='Ingrese el numero de orden de la fase')
     """Numero de orden de la Fase"""
-    Descripcion = models.CharField('Descripcion', max_length=45, help_text='Ingrese la descripcion de la fase')
+    descripcion = models.CharField('Descripcion', max_length=45, help_text='Ingrese la descripcion de la fase')
     """Descripción de la Fase"""
-    FechaCreacion = models.DateField('Fecha de Creacion', help_text='Ingrese la fecha de creacion de la fase')
+    fechacreacion = models.DateField('Fecha de Creacion', help_text='Ingrese la fecha de creacion de la fase')
     """Fecha de creación de la Fase"""
-    FechaInicio = models.DateField('Fecha de Inicio', help_text='Ingrese la fecha de Inicio de la fase', null=True,
+    fechainicio = models.DateField('Fecha de Inicio', help_text='Ingrese la fecha de Inicio de la fase', null=True,
                                    blank=True)
     """Fecha de inicio de la Fase"""
-    FechaFin = models.DateField('Fecha de Fin', help_text='Ingrese la fecha de Fin de la fase', null=True, blank=True)
+    fechafin = models.DateField('Fecha de Fin', help_text='Ingrese la fecha de Fin de la fase', null=True, blank=True)
     """Fecha de finalizaciónde la Fase"""
     Estado_CHOICES = (
         ('I', 'Inicio'),
         ('D', 'Desarrollo'),
         ('F', 'Finalizada'),
     )
-    Estado = models.CharField('Estado', max_length=1, choices=Estado_CHOICES, help_text='Ingrese el Estado la Fase',
+    estado = models.CharField('Estado', max_length=1, choices=Estado_CHOICES, help_text='Ingrese el Estado la Fase',
                               null=True,
                               blank=True, default='I')
     """Estado de la Fase"""
 
     def __unicode__(self):
         """En esta clase definimos como se vera a la instancia de la clase Fase"""
-        return u'%s | %s' % (self.fkproyecto, self.Nombre)
+        return u'%s | %s' % (self.fkproyecto, self.nombre)
 
     class Meta:
         """En esta clase definimos que se listaran las fases ordenados por el nombre"""
-        ordering = ('Nombre',)
+        ordering = ('nombre',)
         verbose_name = u'Fase'
         verbose_name_plural = 'Fases'
 
@@ -101,21 +101,21 @@ class TipoItem(models.Model):
     Clase TipoItem
     Definimos los atributos de la clase TipoItem
     """
-    Fase = models.ForeignKey(Fase, verbose_name="Fase", help_text='Seleccione la Fase')
+    fase = models.ForeignKey(Fase, verbose_name="Fase", help_text='Seleccione la Fase')
     """Nombre de la Fase"""
-    Nombre = models.CharField('Nombre', max_length=45, help_text='Ingrese el nombre del Tipo de Item')
+    nombre = models.CharField('Nombre', max_length=45, help_text='Ingrese el nombre del Tipo de Item')
     """Nombre del Tipo de Item"""
-    Descripcion = models.CharField('Descripcion', max_length=45,
+    descripcion = models.CharField('Descripcion', max_length=45,
                                    help_text='Ingrese la descripcion del Tipo de Item')
     """Descripción del Tipo de Item"""
 
     def __unicode__(self):
         """En esta clase definimos como se vera a la instancia de la clase TipoItem"""
-        return u'%s | %s' % (self.Fase, self.Nombre)
+        return u'%s | %s' % (self.fase, self.nombre)
 
     class Meta:
         """En esta clase definimos que se listaran los Tipos de Item por el nombre"""
-        ordering = ('Nombre',)
+        ordering = ('nombre',)
         verbose_name = u'TipoItem'
         verbose_name_plural = 'Tipos de Item'
 
@@ -125,21 +125,21 @@ class AtributoTipoItem(models.Model):
     Clase AtributoTipoItem
     Definimos los atributos de la clase AtributoTipoItem
     """
-    TipoItem = models.ForeignKey(TipoItem, verbose_name="Tipo de Item", help_text='Seleccione el Tipo de Item')
+    tipoitem = models.ForeignKey(TipoItem, verbose_name="Tipo de Item", help_text='Seleccione el Tipo de Item')
     """Nombre del Tipo de Item"""
-    Nombre = models.CharField('Nombre', max_length=45, help_text='Ingrese el nombre del atributo del Tipo de Item')
+    nombre = models.CharField('Nombre', max_length=45, help_text='Ingrese el nombre del atributo del Tipo de Item')
     """Nombre del atributo del Tipo de Item"""
-    Descripcion = models.CharField('Descripcion', max_length=45,
+    descripcion = models.CharField('Descripcion', max_length=45,
                                    help_text='Ingrese la descripcion del atributo del Tipo de Item')
     """Descripción del atributo del Tipo de Item"""
 
     def __unicode__(self):
         """En esta clase definimos como se vera a la instancia de la clase AtributoTipoItem"""
-        return u'%s | %s' % (self.TipoItem, self.Nombre)
+        return u'%s | %s' % (self.tipoitem, self.nombre)
 
     class Meta:
         """En esta clase definimos que se listaran los Atributos del Tipos de Item por el nombre"""
-        ordering = ('Nombre',)
+        ordering = ('nombre',)
         verbose_name = u'Atributo del Tipo Item'
         verbose_name_plural = 'Atributos del Tipo de Item'
 
@@ -149,17 +149,17 @@ class LineaBase(models.Model):
     Clase LineaBase
     Definimos los atributos de la clase LineaBase
     """
-    Fase = models.ForeignKey(Fase, verbose_name="Fase", help_text='Seleccione la Fase')
+    fase = models.ForeignKey(Fase, verbose_name="Fase", help_text='Seleccione la Fase')
     """Nombre de la Fase"""
-    Nombre = models.CharField('Nombre', max_length=45, help_text='Ingrese el nombre de la Linea Base')
+    nombre = models.CharField('Nombre', max_length=45, help_text='Ingrese el nombre de la Linea Base')
     """Nombre de la Linea Base"""
-    FechaCreacion = models.DateField('Fecha de Creacion', help_text='Ingrese la fecha de creacion de la Linea Base')
+    fechacreacion = models.DateField('Fecha de Creacion', help_text='Ingrese la fecha de creacion de la Linea Base')
     """Fecha de creación de la Linea Base"""
     Estado_CHOICES = (
         ('I', 'Inicida'),
         ('A', 'Aprovada'),
     )
-    Estado = models.CharField('Estado', max_length=1, choices=Estado_CHOICES,
+    estado = models.CharField('Estado', max_length=1, choices=Estado_CHOICES,
                               help_text='Ingrese el Estado la Linea Base',
                               null=True,
                               blank=True, default='I')
@@ -167,11 +167,11 @@ class LineaBase(models.Model):
 
     def __unicode__(self):
         """En esta clase definimos como se vera a la instancia de la clase LineaBase"""
-        return u'%s' % (self.Nombre)
+        return u'%s' % (self.nombre)
 
     class Meta:
         """En esta clase definimos que se listaran las fases ordenados por el nombre"""
-        ordering = ('Nombre',)
+        ordering = ('nombre',)
         verbose_name = u'Linea Base'
         verbose_name_plural = 'Lineas Base'
 
@@ -181,16 +181,16 @@ class Item(models.Model):
     Clase Item
     Definimos los atributos de la clase Item
     """
-    TipoItem = models.ForeignKey(TipoItem, verbose_name="Tipo de Item", help_text='Seleccione el tipo de Item')
+    tipoitem = models.ForeignKey(TipoItem, verbose_name="Tipo de Item", help_text='Seleccione el tipo de Item')
     """Nombre del TipoItem"""
-    LineaBase = models.ForeignKey(LineaBase, verbose_name="Linea Base", help_text='Seleccione la Linea Base', null=True,
+    lineabase = models.ForeignKey(LineaBase, verbose_name="Linea Base", help_text='Seleccione la Linea Base', null=True,
                                   blank=True)
     """Nombre de la Linea Base"""
-    Nombre = models.CharField('Nombre', max_length=45, help_text='Ingrese el nombre de la Item')
+    nombre = models.CharField('Nombre', max_length=45, help_text='Ingrese el nombre de la Item')
     """Nombre del Item"""
-    Descripcion = models.CharField('Descripcion', max_length=45, help_text='Ingrese la descripcion del Item')
+    descripcion = models.CharField('Descripcion', max_length=45, help_text='Ingrese la descripcion del Item')
     """Descripción del Item"""
-    Complejidad = models.IntegerField(help_text='Ingrese la complejidad del Item')
+    complejidad = models.IntegerField(help_text='Ingrese la complejidad del Item')
     """Complejidad del Item"""
     Estado_CHOICES = (
         ('M', 'En Modificacion'),
@@ -200,24 +200,24 @@ class Item(models.Model):
         ('E', 'Eliminado'),
         ('V', 'En revision'),
     )
-    Estado = models.CharField('Estado', max_length=1, choices=Estado_CHOICES, help_text='Ingrese el Estado del Item',
+    estado = models.CharField('Estado', max_length=1, choices=Estado_CHOICES, help_text='Ingrese el Estado del Item',
                               null=True,
                               blank=True, default='M')
     """Estado del Item"""
-    Version = models.IntegerField(help_text='Ingrese la Version del Item')
+    version = models.IntegerField(help_text='Ingrese la Version del Item')
     """Version del Item"""
-    Costo = models.IntegerField(help_text='Ingrese el costo del Item')
+    costo = models.IntegerField(help_text='Ingrese el costo del Item en Dolares $')
     """Costo del Item"""
-    FechaModificacion = models.DateField('Fecha de Modificacion', help_text='Ingrese la fecha de modificacion del Item')
+    fechamodificacion = models.DateField('Fecha de Modificacion', help_text='Ingrese la fecha de modificacion del Item')
     """Fecha de modificacion del Item"""
 
     def __unicode__(self):
         """En esta clase definimos como se vera a la instancia de la clase Item"""
-        return u'%s | %s' % (self.TipoItem, self.Nombre)
+        return u'%s | %s' % (self.tipoitem, self.nombre)
 
     class Meta:
         """En esta clase definimos que se listaran los Item ordenados por el nombre"""
-        ordering = ('Nombre',)
+        ordering = ('nombre',)
         verbose_name = u'Item'
         verbose_name_plural = 'Items'
 
@@ -227,24 +227,24 @@ class AtributoItem(models.Model):
     Clase AtributoItem
     Definimos los atributos de la clase AtributoItem
     """
-    Item = models.ForeignKey(Item, verbose_name="Item", help_text='Seleccione el  Item')
+    item = models.ForeignKey(Item, verbose_name="Item", help_text='Seleccione el  Item')
     """Nombre del Item"""
-    AtributoTipoItem = models.ForeignKey(AtributoTipoItem, verbose_name="Atributo del Tipo de Item",
+    atributotipoitem = models.ForeignKey(AtributoTipoItem, verbose_name="Atributo del Tipo de Item",
                                          help_text='Seleccione el  Atributo del Tipo de Item')
     """Nombre del Atributo del Tipo de Item"""
-    Nombre = models.CharField('Nombre', max_length=45, help_text='Ingrese el nombre del atributo del Item')
+    nombre = models.CharField('Nombre', max_length=45, help_text='Ingrese el nombre del atributo del Item')
     """Nombre del atributo del Item"""
-    Descripcion = models.CharField('Descripcion', max_length=45,
+    descripcion = models.CharField('Descripcion', max_length=45,
                                    help_text='Ingrese la descripcion del atributo del Item')
     """Descripción del atributo del Item"""
 
     def __unicode__(self):
         """En esta clase definimos como se vera a la instancia de la clase AtributoItem"""
-        return u'%s | %s' % (self.Item, self.Nombre)
+        return u'%s | %s' % (self.item, self.nombre)
 
     class Meta:
         """En esta clase definimos que se listaran los Atributos del Item por el nombre"""
-        ordering = ('Nombre',)
+        ordering = ('nombre',)
         verbose_name = u'Atributo del Item'
         verbose_name_plural = 'Atributos del Item'
 
@@ -254,7 +254,7 @@ class RelacionItem(models.Model):
     Clase RelacionItem
     Definimos los atributos de la clase RelacionItem
     """
-    ItemOrigen = models.ForeignKey(Item, verbose_name="Item Origen", help_text='Seleccione el primer Item Origen',
+    itemorigen = models.ForeignKey(Item, verbose_name="Item Origen", help_text='Seleccione el primer Item Origen',
                                    related_name="ItemOrigen")
     """Nombre del Item Origen"""
 
@@ -262,21 +262,21 @@ class RelacionItem(models.Model):
         ('P', 'Pade - Hijo'),
         ('A', 'Antecesor - Sucesor'),
     )
-    TipoRelacion = models.CharField('Tipo de Relacion', max_length=1, choices=TipoRelacion_CHOICES,
+    tiporelacion = models.CharField('Tipo de Relacion', max_length=1, choices=TipoRelacion_CHOICES,
                                     help_text='Ingrese el Tipo de Relacion de los Items')
     """Tipo de Relacion Item"""
-    ItemDestino = models.ForeignKey(Item, verbose_name="Item Destino", help_text='Seleccione el Item Destino',
+    itemdestino = models.ForeignKey(Item, verbose_name="Item Destino", help_text='Seleccione el Item Destino',
                                     related_name="ItemDestino")
     """Nombre del Item Destino"""
 
 
     def __unicode__(self):
         """En esta clase definimos como se vera a la instancia de la clase RelacionItem"""
-        return u'%s | %s | %s' % (self.ItemOrigen, self.TipoRelacion, self.ItemDestino)
+        return u'%s | %s | %s' % (self.itemorigen, self.tiporelacion, self.itemdestino)
 
     class Meta:
         """En esta clase definimos que se listaran las relaciones de los Item ordenados por el Item Origen"""
-        ordering = ('ItemOrigen', 'ItemDestino',)
+        ordering = ('itemorigen', 'itemdestino',)
         verbose_name = u'Relacion Item'
         verbose_name_plural = 'Relacion Items'
 
